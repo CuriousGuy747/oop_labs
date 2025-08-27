d2fefd4d06134870a10f3c9cb8e523f494c5c73c
CuriousGuy747 <s.shamsiddinov@newuu.uz>
2025-08-27 13:23:14 +0500

    hi

---
+++ oop_lab1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Shahriddin Shamsiddinov on 27/08/25.
+//
+#include <iostream>;
+using namespace std;
+
+int main(){
+    cout << "Hello world" << endl;
+    for(int i = 1; i < 10; i++){
+        cout << i << endl;
+    }
+}
