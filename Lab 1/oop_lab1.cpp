fe6b50bf74c639a562f571408a0521273e8dfa79
CuriousGuy747 <s.shamsiddinov@newuu.uz>
2025-08-27 14:47:49 +0500

    Update oop_lab1.cpp

--- Lab 1/oop_lab1.cpp
+++ Lab 1/oop_lab1.cpp
@@ -12,19 +12,19 @@ int main(){
     // //     cout << i << endl;
     // // }
     //
-    // // task1
-    // int number;
-    // int sum = 0;
-    // cout << "Input number: ";
-    // cin >> number;
-    // cout << endl;
-    //
-    // while (number > 0) {
-    //     int r = number % 10;
-    //     number = number / 10;
-    //     sum += r;
-    // }
-    // cout << sum << endl;
+    // task1
+    int number;
+    int sum = 0;
+    cout << "Input number: ";
+    cin >> number;
+    cout << endl;
+    
+    while (number > 0) {
+        int r = number % 10;
+        number = number / 10;
+        sum += r;
+    }
+    cout << sum << endl;
 
 
 
